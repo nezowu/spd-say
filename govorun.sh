7c3401d19ec316b0fb787a954accd23f9232f962
olecya <gxendz@mail.ru>
2016-10-03 23:12:53 +0300

    three

--- govorun.sh
+++ govorun.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 count=1
-spd-say -l ru "считаю додеся тии и стреляю"
+#spd-say -l ru "считаю додеся тии и стреляю"
+espeak -v ru "считаю додеся тии и стреляю"
 sleep 2
 while [[ "$count" -lt "11" ]]; do
 	espeak -v de "$count";
 	((count++));
 done
-mplayer -ao pulse mp40.mp3
+#mplayer -ao pulse mp40.mp3
+mplayer mp40.mp3
